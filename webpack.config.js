fc93c8bee84fc5f3c38bfeff3145548a7e33075f
Ronan-Codes <galv.ronan@gmail.com>
2021-07-14 02:36:37 -0400

    Completed webpack for 19.2, run Lighthouse

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
+// used in plugin below
+const webpack = require("webpack");
 // Review
 const path = require("path");
-
 
 module.exports = {
     entry: './assets/js/script.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
+    // npm i jQuery 1st
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
     mode: 'development'
 };
