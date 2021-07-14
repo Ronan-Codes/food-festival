068d8dfada64587cfce8f63a3c93245bf3fd1e65
Ronan-Codes <galv.ronan@gmail.com>
2021-07-14 03:01:33 -0400

    Use new starter code,code-snapshot

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,17 @@
-// used in plugin below
-const webpack = require("webpack");
-// Review
+const webpack = require("webpack")
 const path = require("path");
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
-    // npm i jQuery 1st
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        }),
-    ],
-    mode: 'development'
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  ],
+  mode: "development"
 };
