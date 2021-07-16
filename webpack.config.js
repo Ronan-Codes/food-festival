9726f03b8d747a757eadd2111c77d8484df85b7c
Ronan-Codes <galv.ronan@gmail.com>
2021-07-16 03:42:23 -0400

    optimize 2 images and javascript

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,58 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 const webpack = require("webpack")
 const path = require("path");
 
 module.exports = {
-  entry: "./assets/js/script.js",
+  // entry: "./assets/js/script.js",
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
   output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "main.bundle.js"
+    // path: path.join(__dirname + "/dist"),
+    // filename: "main.bundle.js"
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        // Review
+        test: /\.jpg$/i,
+        use: [
+          {
+            // file-loader application
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              // returns name of file with file ext.
+              name (file){
+                return "[path][name].[ext]"
+              },
+              // changes our assignment URL by replacing ../ from require() statement with /assets/
+              publicPath: function(url){
+                return url.replace("../", "/assets/")
+              }
+            }
+          },
+          {
+            // file-loader processes image first then iwl optimizes emitted files
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
   ],
   mode: "development"
 };
